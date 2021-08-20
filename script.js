4ce4b99fe25a8d3bc514588bac30cd98e1a268a8
Ksenia <shalando@ya.ru>
2021-08-20 22:52:02 +0300

    three commit

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
- var nameI = prompt('Введите ваше имя','Иван');
- var surname = prompt('Введите вашу фамилию','Иванович');
- var patronymic = prompt('Введите ваше отчество','Иванов');
-
+ var nameI ='';
+    do {
+    nameI = prompt('Введите ваше имя','Иван');
+    } while(nameI==='');
+ var surname = '';
+    do {
+    surname = prompt('Введите вашу фамилию','Иванович');
+    } while(surname==='');
+ var patronymic = '';
+    do {
+    patronymic = prompt('Введите ваше отчество','Иванов');
+    } while(patronymic==='');
  var age = prompt('Введите ваш возраст в годах','20');
- var age = parseFloat(age);
+ age = parseFloat(age);
  var ageP = 'Result';
- if(age>=65) { 
-    ageP="да";
-}
- else {
-    ageP="нет";
-}
+    if(age>=65) { 
+        ageP='да';
+    }
+    else {
+        ageP='нет';
+    } 
  var gender = confirm ("Ваш пол - мужской? Если женский - нажмите 'нет'");
- if(gender) { 
-    gender="мужской";
-} 
- else {
-    gender="женский";
-}
+    if(gender) { 
+        gender='мужской';
+    } 
+    else {
+        gender='женский';
+    }
 
-alert ('ваше ФИО: ' + nameI +' ' + surname +' '+ patronymic + 
+
+ alert ('ваше ФИО: ' + nameI +' ' + surname +' '+ patronymic + 
 ' ваш возраст в годах: '+ age +
 ' ваш возраст в днях: '+(age*365)+
 ' через 5 лет вам будет: '+(age+5) + 
